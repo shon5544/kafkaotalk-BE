437b458067572f930275719ec2adc6ef95802d97
shon5544 <sbs8239@gmail.com>
2023-12-31 00:37:53 +0900

    feat: user 엔티티 추가

---
+++ src/main/kotlin/com/beomsu/kafkaotalkbe/friends/domain/User.kt
@@ -0,0 +1,35 @@
+package com.beomsu.kafkaotalkbe.friends.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
+
+@Entity
+class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, name = "user_id")
+    private val id: Long? = null,
+
+    var email: String = "",
+    var password: String = "",
+    var username: String = "",
+    var refreshToken: String = "",
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    var friend: User?,
+
+    @OneToMany
+    var friendList: ArrayList<User>?,
+
+    @Column(name = "profile_image")
+    var profileImg: String?,
+
+    @Column(name = "background_image")
+    var bgImg: String?
+)
