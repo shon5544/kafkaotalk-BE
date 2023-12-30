b6da9cac5662d298524090eb36bcb885c8e06bb4
shon5544 <sbs8239@gmail.com>
2023-12-31 01:33:07 +0900

    feat: 의존성 역전

--- src/main/kotlin/com/beomsu/kafkaotalkbe/global/domain/ICustomLocalDateTime.kt
+++ src/main/kotlin/com/beomsu/kafkaotalkbe/global/domain/ICustomLocalDateTime.kt
@@ -1,4 +1,20 @@
 package com.beomsu.kafkaotalkbe.global.domain
 
+import org.springframework.stereotype.Component
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+
 interface ICustomLocalDateTime {
+    fun plusHour(hours: Long): Instant
+}
+
+@Component
+class CustomLocalDateTime: ICustomLocalDateTime {
+    override fun plusHour(hours: Long): Instant {
+        return LocalDateTime.now()
+            .plusHours(hours)
+            .atZone(ZoneId.of("Asia/Seoul"))
+            .toInstant()
+    }
 }
