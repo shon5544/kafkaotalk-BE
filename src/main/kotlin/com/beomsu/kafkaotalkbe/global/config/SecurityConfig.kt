b6cb32cd727b6eb5bb156624b791299536049a0a
shon5544 <sbs8239@gmail.com>
2023-12-31 00:58:52 +0900

    feat: 필터체인 프레임 추가

---
+++ src/main/kotlin/com/beomsu/kafkaotalkbe/global/config/SecurityConfig.kt
@@ -0,0 +1,39 @@
+package com.beomsu.kafkaotalkbe.global.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    @Value("\${jwt.access.header}")
+    private val accessTokenHeader: String,
+
+    @Value("\${jwt.access.refresh}")
+    private val refreshTokenHeader: String
+) {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .formLogin { it.disable() }
+            .httpBasic { it.disable() }
+            .csrf { it.disable() }
+            .headers {
+                it.frameOptions {
+                    it.disable()
+                }
+            }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests {
+                it.anyRequest().permitAll()
+            }
+
+        return http.build()
+    }
+}
