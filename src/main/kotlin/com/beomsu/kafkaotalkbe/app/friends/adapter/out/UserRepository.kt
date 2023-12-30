bdd57628580dde29f8e5b91edb6e382a4c963dcf
shon5544 <sbs8239@gmail.com>
2023-12-31 01:17:33 +0900

    feat: UserRepository 네이밍 수정

--- src/main/kotlin/com/beomsu/kafkaotalkbe/app/friends/adapter/out/UserRepository.kt
+++ src/main/kotlin/com/beomsu/kafkaotalkbe/app/friends/adapter/out/UserRepository.kt
@@ -1,14 +1,13 @@
 package com.beomsu.kafkaotalkbe.app.friends.adapter.out
 
+import com.beomsu.kafkaotalkbe.app.friends.application.port.out.UserPersistencePort
 import com.beomsu.kafkaotalkbe.app.friends.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-interface UserRepository {
-}
-
 @Repository
-class UserRepositoryImpl(private val userRepository: UserJpaRepository): UserRepository {
+class UserRepository(private val userJpaRepository: UserJpaRepository):
+    UserPersistencePort {
 
 }
 
