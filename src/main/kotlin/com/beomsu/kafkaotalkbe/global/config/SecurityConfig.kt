b9aa3e390c27c03e88b0e6c2942314d3d1af7749
shon5544 <sbs8239@gmail.com>
2023-12-31 01:00:11 +0900

    feat: passwordEncoder 추가

--- src/main/kotlin/com/beomsu/kafkaotalkbe/global/config/SecurityConfig.kt
+++ src/main/kotlin/com/beomsu/kafkaotalkbe/global/config/SecurityConfig.kt
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -35,5 +37,10 @@ class SecurityConfig(
             }
 
         return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
     }
 }
