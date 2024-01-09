d11bf3697f2007ebb08bd25051c452660cac76a2
shon5544 <sbs8239@gmail.com>
2024-01-09 19:38:24 +0900

    feat: uri 전달용 데이터 객체

---
+++ src/main/kotlin/com/beomsu/kafkaotalkbe/global/common/RequestURI.kt
@@ -0,0 +1,9 @@
+package com.beomsu.kafkaotalkbe.global.common
+
+class RequestURI {
+    companion object {
+        const val USER = "/user"
+        const val ARTICLE = "/article"
+        const val COMMENT = "/comment"
+    }
+}
